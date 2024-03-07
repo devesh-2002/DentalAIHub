5339c37fdf90055083b19d6e112a2d3c02062896
Devesh R <dev241202@gmail.com>
2024-03-07 23:51:11 +0530

    Added frontend for medicalproduct

--- dentalAI/dentalAI/settings.py
+++ dentalAI/dentalAI/settings.py
@@ -97,9 +97,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATABASES = {
     'default': {
